0b3f866787bdc6cfce5f500e499535d205763031
Capi Etheriel <barraponto@gmail.com>
2013-10-11 17:13:11 -0300

    Material para a aula 3.

---
+++ 03-Primeiros-passos/oi-mundo.php
@@ -0,0 +1,12 @@
+<?php $texto = "Oi Mundo!"; ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>COLOQUE O TITULO AQUI</title>
+  </head>
+  <body>
+  <p><?php print $texto; ?></p>
+  </body>
+</html>
