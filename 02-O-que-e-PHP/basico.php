15be4708e42a9a4fd072517a1a3e9fe0d2743692
Capi Etheriel <barraponto@gmail.com>
2013-10-11 17:12:58 -0300

    Material para a aula 2.

---
+++ 02-O-que-e-PHP/basico.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>O que é PHP</title>
+  </head>
+  <body>
+  <p>Meu nome é <?php print "Capi Etheriel"; ?>.</p>
+  </body>
+</html>
