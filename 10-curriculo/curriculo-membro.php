9bf471c0a42373bafc4a355bc67f0346544fedad
Capi Etheriel <barraponto@gmail.com>
2013-11-28 21:54:32 -0200

    aula 10: unidade 2 (com atualizações no código exemplo)

--- 10-curriculo/curriculo-membro.php
+++ 10-curriculo/curriculo-membro.php
@@ -1,29 +1,19 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $nome ?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-  </head>
-  <body>
-    <div class="container">
-    <div class="jumbotron clearfix">
-      <h1>
-        Olá, eu sou <?= $nome ?>.
-        <span class="small">Eu sou um <?= $profissao ?> com foco em <?= $especialidade ?>.</span>
-      </h1>
-      <div class="pull-right">
-        <?php foreach ($contato as $canal => $url): ?>
-          <a class="btn btn-primary btn-lg" href="<?= $url ?>">
-            <?= $canal ?>
-          </a>
-        <?php endforeach; ?>
-      </div>
-    </div>
+<div class="jumbotron clearfix">
+  <h1>
+    Olá, eu sou <?= $nome ?>.
+    <span class="small">Eu sou um <?= $profissao ?> com foco em <?= $especialidade ?>.</span>
+  </h1>
+  <div class="pull-right">
+    <?php foreach ($contato as $canal => $url): ?>
+      <a class="btn btn-primary btn-lg" href="<?= $url ?>">
+        <?= $canal ?>
+      </a>
+    <?php endforeach; ?>
+  </div>
+</div>
 
-    <div class="row">
-    <div class="col-sm-12 col-md-4">
+<div class="row">
+  <div class="col-sm-12 col-md-4">
     <h2>Formação e Experiência</h2>
     <?php
       foreach (array_merge($formacao, $experiencia) as $item): ?>
@@ -35,46 +25,43 @@
     <?php endforeach; ?>
     </div><!-- /col -->
 
-    <div class="col-sm-6 col-md-4">
-    <h2>Portfolio</h2>
-    <?php foreach ($portfolio as $item): ?>
-      <div class="media">
+  <div class="col-sm-6 col-md-4">
+  <h2>Portfolio</h2>
+  <?php foreach ($portfolio as $item): ?>
+    <div class="media">
 
-        <?php if (!empty($item['foto'])): ?>
-          <figure class="pull-left">
-            <img class="media-object" src="<?= $item['foto'] ?>" />
-          </figure>
-        <?php endif; ?>
+      <?php if (!empty($item['foto'])): ?>
+        <figure class="pull-left">
+          <img class="media-object" src="<?= $item['foto'] ?>" />
+        </figure>
+      <?php endif; ?>
 
-        <div class="media-body">
-          <h3 class="media-heading">
-            <?= $item['titulo'] ?>
-              <small>
-                <?= $item['ano'] ?>
-                <?php if (!empty($item['url'])): ?>
-                  &mdash;
-                  <a href="<?= $item['url'] ?>">Ver site</a>
-                <?php endif; ?>
-              </small>
-          </h3>
-        </div><!-- /media-body -->
+      <div class="media-body">
+        <h3 class="media-heading">
+          <?= $item['titulo'] ?>
+            <small>
+              <?= $item['ano'] ?>
+              <?php if (!empty($item['url'])): ?>
+                &mdash;
+                <a href="<?= $item['url'] ?>">Ver site</a>
+              <?php endif; ?>
+            </small>
+        </h3>
+      </div><!-- /media-body -->
 
-      </div><!-- /media -->
-    <?php endforeach; ?>
-    </div><!-- /col -->
+    </div><!-- /media -->
+  <?php endforeach; ?>
+  </div><!-- /col -->
 
-    <div class="col-sm-6 col-md-4">
-    <h2>Proficiências e Habilidades</h2>
-      <?php foreach ($profhabil as $habilidade => $valor): ?>
-        <div class="progress">
-          <div class="progress-bar" style="width:<?= $valor ?>%">
-            <?= $habilidade ?>
-            <span class="sr-only"><?= $habilidade . ': ' . $valor . '%' ?></span>
-          </div>
+  <div class="col-sm-6 col-md-4">
+  <h2>Proficiências e Habilidades</h2>
+    <?php foreach ($profhabil as $habilidade => $valor): ?>
+      <div class="progress">
+        <div class="progress-bar" style="width:<?= $valor ?>%">
+          <?= $habilidade ?>
+          <span class="sr-only"><?= $habilidade . ': ' . $valor . '%' ?></span>
         </div>
-      <?php endforeach; ?>
-    </div><!-- col -->
-  </div><!-- row -->
-  </div><!-- container -->
-  </body>
-</html>
+      </div>
+    <?php endforeach; ?>
+  </div><!-- col -->
+</div><!-- row -->
---
+++ 10-curriculo/layout.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $nome ?></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+  </head>
+  <body>
+    <div class="container">
+    </div><!-- /container -->
+  </body>
+</html>
