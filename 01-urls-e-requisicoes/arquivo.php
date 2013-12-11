f03b10af71c2e8c2d4bd986f1cc161384657db7e
Capi Etheriel <barraponto@gmail.com>
2013-12-11 19:57:27 -0200

    aula 1: material

---
+++ 01-urls-e-requisicoes/arquivo.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>O que é PHP</title>
+  </head>
+  <body>
+  <p>Bem vindo ao curso sobre PHP.</p>
+  <p>
+    Agora são <?php print date('H:i:s'); ?> no
+    <a href="http://pt.wikipedia.org/wiki/UTC">
+      Tempo Universal Coordenado (UTC)
+    </a>.
+  </p>
+  </body>
+</html>
